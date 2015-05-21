0bd3ab5398a51eff3fe21d84f7d4a5bd90d82add
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2015-05-21 16:09:24 +0200

    - changed export menu key to 'n' to avoid clash with 'm' for markdown export

--- ox-mediawiki.el
+++ ox-mediawiki.el
@@ -93,7 +93,7 @@ by the footnotes themselves."
   :export-block '("MW" "MEDIAWIKI")
   :filters-alist '((:filter-parse-tree . org-mw-separate-elements))
   :menu-entry
-  '(?m "Export to Mediawiki"
+  '(?n "Export to Mediawiki"
        ((?M "To temporary buffer"
             (lambda (a s v b) (org-mw-export-as-mediawiki a s v)))
         (?m "To file" (lambda (a s v b) (org-mw-export-to-mediawiki a s v)))
